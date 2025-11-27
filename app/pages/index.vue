49199a7cbe411b6a18583e43a701e43824d536f2
marzieh.dolati@dayasoft.com <marzieh.dolati@dayasoft.com>
2025-11-27 10:33:03 +0100

    feat: 🎸 basic components

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <UContainer class="py-20 flex flex-col items-center gap-10">
+    <HeroTitle name="Marzieh" text="rfgrgrfg"/>
+
+
+
+
+  </UContainer>
+</template>
+
+<script setup>
+
+// Nothing needed here
+</script>
