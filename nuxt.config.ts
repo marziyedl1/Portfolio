a55633ce5eff59005e26d810c7d6dc5b2ac4e826
marzieh.dolati@dayasoft.com <marzieh.dolati@dayasoft.com>
2025-11-27 10:20:48 +0100

    feat: 🎸 install basis

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui', '@nuxtjs/i18n'],
+  css: ['~/assets/css/main.css'],
+  i18n: {
+    defaultLocale: 'en',
+    locales: [
+      { code: 'en', name: 'English', file: 'en.json' },
+      { code: 'de', iso: 'de-DE', file: 'de.json', name: 'Deutsch' }
+    ],
+    langDir: 'locales/',
+
+  }
+
+
+})
